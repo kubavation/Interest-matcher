bea2c712ba77763296558865eb52d9c381dc2dda
kubavation <kubavation@wp.pl>
2019-06-28 23:20:54 +0200

    Discovery-service init

---
+++ DicoveryService/src/main/java/io/duryskuba/interestmatcher/DicoveryService/DicoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.duryskuba.interestmatcher.DicoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryServiceApplication.class, args);
+	}
+
+}
---
+++ DicoveryService/src/test/java/io/duryskuba/interestmatcher/DicoveryService/DicoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.duryskuba.interestmatcher.DicoveryService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DicoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
