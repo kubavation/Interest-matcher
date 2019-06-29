7f72cbe5b16d826982235377c97fd60b87ff3d50
kubavation <kubavation@wp.pl>
2019-06-29 17:11:32 +0200

    working axon dashboard

--- UserCommandService/src/main/java/io/duryskuba/interestmatcher/UserCommandService/controller/UserCommandController.java
+++ UserCommandService/src/main/java/io/duryskuba/interestmatcher/UserCommandService/controller/UserCommandController.java
@@ -4,7 +4,10 @@ import io.duryskuba.interestmatcher.UserCommandService.command.CreateUserCommand
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 public class UserCommandController {
@@ -12,7 +15,12 @@ public class UserCommandController {
     @Autowired
     CommandGateway commandGateway;
 
-    @GetMapping("/user")
+    @PostConstruct
+    public void x( ) {
+        System.out.println("XDD");
+    }
+
+    @PostMapping("/user")
     public void test() {
         System.out.println("in controller");
         commandGateway.send(new CreateUserCommand(1L,"kuba","durys"));
