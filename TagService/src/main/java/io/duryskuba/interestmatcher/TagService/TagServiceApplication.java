6530e1fa69f1dba52904db54a69253baa6e657a2
kubavation <kubavation@wp.pl>
2019-07-01 15:30:29 +0200

    init config in tag-service

---
+++ TagService/src/main/java/io/duryskuba/interestmatcher/TagService/TagServiceApplication.java
@@ -0,0 +1,13 @@
+package io.duryskuba.interestmatcher.TagService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagServiceApplication.class, args);
+	}
+
+}
