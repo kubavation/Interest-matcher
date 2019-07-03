855b48145312a35291a53cb7686b50d2bc6ca7cc
kubavation <kubavation@wp.pl>
2019-07-03 18:26:20 +0200

    init notificationService

---
+++ NotificationService/src/main/kotlin/io/duryskuba/interstmatcher/NotificationService/NotificationServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.duryskuba.interstmatcher.NotificationService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationServiceApplication>(*args)
+}
