fac9b0a0f7a2319a59ac75282851055781e4dc2a
kubavation <kubavation@wp.pl>
2019-07-03 19:39:26 +0200

    init event listener

--- NotificationService/src/main/kotlin/io/duryskuba/interstmatcher/NotificationService/NotificationEventProcessor.kt
+++
@@ -1,7 +0,0 @@
-package io.duryskuba.interstmatcher.NotificationService
-
-import org.springframework.stereotype.Component
-
-@Component
-class NotificationEventProcessor {
-}
---
+++ NotificationService/src/main/kotlin/io/duryskuba/interstmatcher/NotificationService/event/NotificationEventProcessor.kt
@@ -0,0 +1,22 @@
+package io.duryskuba.interstmatcher.NotificationService.event
+
+import io.duryskuba.interstmatcher.NotificationService.resource.NotificationDTO
+import io.duryskuba.interstmatcher.NotificationService.service.NotificationService
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+
+@Component
+class NotificationEventProcessor(
+        val rabbitTemplate: RabbitTemplate,
+        val notifcationService: NotificationService) {
+
+
+    @RabbitListener(queues = arrayOf("notificationQueue"))
+    fun listenAndNotify(notificationDTO: NotificationDTO) {
+        print("here")
+        print(notificationDTO.toString())
+    }
+
+}
