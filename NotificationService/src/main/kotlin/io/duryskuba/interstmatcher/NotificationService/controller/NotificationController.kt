9f658fe6b00f130c60725e7529f05b00f9836f8c
kubavation <kubavation@wp.pl>
2019-07-04 08:16:53 +0200

    new methods in notification-service

--- NotificationService/src/main/kotlin/io/duryskuba/interstmatcher/NotificationService/controller/NotificationController.kt
+++ NotificationService/src/main/kotlin/io/duryskuba/interstmatcher/NotificationService/controller/NotificationController.kt
@@ -1,8 +1,13 @@
 package io.duryskuba.interstmatcher.NotificationService.controller
 
 import io.duryskuba.interstmatcher.NotificationService.service.NotificationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class NotificationController(val notificationService: NotificationService) {
+
+    @GetMapping("/notifications/users/{id}")
+    fun findByUser(@PathVariable Long id) = notificationService.findByUser(id);
 }
