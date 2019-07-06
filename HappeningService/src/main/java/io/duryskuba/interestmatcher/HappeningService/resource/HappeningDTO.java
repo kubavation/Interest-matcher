d179262117b35fb4d13c7aac538f7d1b1b562a80
kubavation <kubavation@wp.pl>
2019-07-06 12:07:51 +0200

    changes in models

--- HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/resource/HappeningDTO.java
+++ HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/resource/HappeningDTO.java
@@ -2,17 +2,28 @@ package io.duryskuba.interestmatcher.HappeningService.resource;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class HappeningDTO {
 
     private String happeningId;
     private String name;
     private String description;
-    private String localeStr;
+
+    private Long numOfParticipants;
+    private Long maxNumOfParticipants;
+    private Long minNumOfParticipants;
+
+    private LocalDateTime when;
+
+    private LocationDTO location;
 
 
 }
--- HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/resource/LocationDTO.java
+++ HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/resource/LocationDTO.java
@@ -2,15 +2,20 @@ package io.duryskuba.interestmatcher.HappeningService.resource;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class LocationDTO {
 
     private Long locationId;
     private String country;
     private String city;
     private String street;
+
+    private double lat;
+    private double lan;
 }
