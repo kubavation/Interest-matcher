b41fed9a605d10d6d8ab0ef1bce4c761ea11c3a0
kubavation <kubavation@wp.pl>
2019-07-06 12:12:41 +0200

    init locationService

---
+++ LocationService/src/main/kotlin/io/duryskuba/interestmatcher/LocationService/LocationServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.duryskuba.interestmatcher.LocationService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocationServiceApplication>(*args)
+}
