89a627fad4f87fd1f9b0cac452333a64fa0d55d7
kubavation <kubavation@wp.pl>
2019-07-09 19:07:41 +0200

    added required methods in user-service

--- UserService/src/main/java/io/duryskuba/interestmatcher/UserService/service/UserService.java
+++ UserService/src/main/java/io/duryskuba/interestmatcher/UserService/service/UserService.java
@@ -2,15 +2,21 @@ package io.duryskuba.interestmatcher.UserService.service;
 
 import io.duryskuba.interestmatcher.UserService.converter.UserConverter;
 import io.duryskuba.interestmatcher.UserService.repository.UserRepository;
+import io.duryskuba.interestmatcher.UserService.resource.User;
 import io.duryskuba.interestmatcher.UserService.resource.UserDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+
+import static io.duryskuba.interestmatcher.UserService.converter.UserConverter.toDto;
+import static io.duryskuba.interestmatcher.UserService.converter.UserConverter.toEntity;
 
 @Service
 public class UserService {
@@ -35,10 +41,32 @@ public class UserService {
      * @return page of users
      */
     public Page<UserDto> findAll(int from, int offset) {
-        return this.userRepository.findAll(PageRequest.of(from, offset))
+        return userRepository.findAll(PageRequest.of(from, offset))
                     .map(UserConverter::toDto);
     }
 
-    public 
+    public Optional<UserDto> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                    .map(UserConverter::toDto);
+    }
+
+    public UserDto findByUsernameOrThrow(String username) {
+        return userRepository.findByUsername(username)
+                    .map(UserConverter::toDto)
+                    .orElseThrow(RuntimeException::new); //resourcenotfound
+    }
+
+    @Transactional
+    public UserDto create(UserDto userDto) {
+        assertIfUserIsAvailable(userDto);
+        return toDto(userRepository.save(toEntity(userDto)));
+    }
+
+    public void assertIfUserIsAvailable(UserDto userDto) {
+        findByUsername(userDto.getUsername())
+                .ifPresent(u -> {
+                    throw new RuntimeException("todo usernametakenexception");
+                });
+    }
 
 }
