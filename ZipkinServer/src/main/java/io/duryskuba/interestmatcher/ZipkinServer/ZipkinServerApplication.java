74db92ea0be21afa27779362c5c241928df6584b
kubavation <kubavation@wp.pl>
2019-07-10 20:35:56 +0200

    init zipkin-server

---
+++ ZipkinServer/src/main/java/io/duryskuba/interestmatcher/ZipkinServer/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package io.duryskuba.interestmatcher.ZipkinServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
