7ba34963ddbae430a8da0f0f00e47c7196866bdf
kubavation <kubavation@wp.pl>
2019-07-14 14:40:33 +0200

    init crud operations in post-service

--- PostService/src/main/java/io/duryskuba/interestmatcher/PostService/service/PostService.java
+++ PostService/src/main/java/io/duryskuba/interestmatcher/PostService/service/PostService.java
@@ -3,9 +3,14 @@ package io.duryskuba.interestmatcher.PostService.service;
 import io.duryskuba.interestmatcher.PostService.repository.PostRepository;
 import io.duryskuba.interestmatcher.PostService.resource.Post;
 import io.duryskuba.interestmatcher.PostService.resource.PostDto;
+import io.duryskuba.interestmatcher.PostService.util.PostConverter;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+
+import static io.duryskuba.interestmatcher.PostService.util.PostConverter.toDTO;
+import static io.duryskuba.interestmatcher.PostService.util.PostConverter.toDTOCollection;
 
 @Service
 public class PostService {
@@ -17,9 +22,15 @@ public class PostService {
     }
 
 
-//    public List<PostDto> findAll() {
-//        postRepository.findAll();
-//    }
+    public Collection<PostDto> findAll() {
+      return toDTOCollection( postRepository.findAll() );
+    }
+
+    public PostDto findById(Long id) {
+        return toDTO( postRepository.findById(id)
+                        .orElseThrow(RuntimeException::new) );
+    }
+
 
     public Post create(Post post) {
         return postRepository.save(post);
