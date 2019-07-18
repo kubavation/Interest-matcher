eb542377d4fbe0669864bdc40a77bdbcc981e56b
kubavation <kubavation@wp.pl>
2019-07-18 19:22:35 +0200

    changes in participantAction enum ( BiConsumer added )

--- HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/enums/ParticipantAction.java
+++ HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/enums/ParticipantAction.java
@@ -4,26 +4,22 @@ import io.duryskuba.interestmatcher.HappeningService.resource.HappeningDTO;
 import io.duryskuba.interestmatcher.HappeningService.resource.HappeningParticipantDTO;
 import io.duryskuba.interestmatcher.HappeningService.service.HappeningService;
 
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public enum ParticipantAction {
-//
-//    ADD(HappeningService::addParticipantToHappening);
-//   // REMOVE(HappeningService::removeParticipantFromHappening);
-//
-//    //private BiFunction<HappeningService, HappeningParticipantDTO, HappeningParticipantDTO> func;
-//    private Consumer<HappeningService> func;
-////
-////    ParticipantAction(BiFunction<HappeningService, HappeningParticipantDTO, HappeningParticipantDTO> func) {
-////        this.func = func;
-////    }
-////
-////    public BiFunction<HappeningService, HappeningParticipantDTO, HappeningParticipantDTO> getFunc() {
-////        return func;
-////    }
-//
-//    ParticipantAction(Consumer<HappeningService> func) {
-//        this.func = func;
-//    }
+
+    ADD(HappeningService::addParticipantToHappeningv2),
+    REMOVE(HappeningService::removeParticipantFromHappening);
+
+    private final BiConsumer<HappeningService, HappeningParticipantDTO> func;
+
+    ParticipantAction(BiConsumer<HappeningService, HappeningParticipantDTO> func) {
+        this.func = func;
+    }
+
+    public BiConsumer<HappeningService, HappeningParticipantDTO> getFunc() {
+        return func;
+    }
 }
