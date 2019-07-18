bf555cc199a683555e8d7e9ce7af0a2807a4b456
kubavation <kubavation@wp.pl>
2019-07-18 19:05:53 +0200

    event publisher added in happening-service

--- HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/event/HappeningEventProcessor.java
+++ HappeningService/src/main/java/io/duryskuba/interestmatcher/HappeningService/event/HappeningEventProcessor.java
@@ -1,17 +1,23 @@
 package io.duryskuba.interestmatcher.HappeningService.event;
 
 import io.duryskuba.interestmatcher.HappeningService.service.HappeningService;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HappeningEventProcessor {
 
     private HappeningService happeningService;
+    private ApplicationEventPublisher applicationEventPublisher;
 
-    public HappeningEventProcessor(HappeningService happeningService) {
+    public HappeningEventProcessor(HappeningService happeningService,
+                                   ApplicationEventPublisher applicationEventPublisher) {
         this.happeningService = happeningService;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
 
-    public void manipulateParticipants()
+    public void manipulateParticipants(ParticipantManipulationEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
 }
