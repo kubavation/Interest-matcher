2bea8857da62a127975ae03f459bcedac33052f5
kubavation <kubavation@wp.pl>
2019-07-19 09:47:05 +0200

    init achievement-service

---
+++ AchievementService/src/main/java/io/duryskuba/interestmatcher/AchievementService/AchievementServiceApplication.java
@@ -0,0 +1,13 @@
+package io.duryskuba.interestmatcher.AchievementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievementServiceApplication.class, args);
+	}
+
+}
