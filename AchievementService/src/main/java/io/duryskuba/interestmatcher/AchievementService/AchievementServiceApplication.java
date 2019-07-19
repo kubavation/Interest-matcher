1da457b27e01030b25c3eca310534094b406a6f7
kubavation <kubavation@wp.pl>
2019-07-19 09:51:58 +0200

    eureka client added

--- AchievementService/src/main/java/io/duryskuba/interestmatcher/AchievementService/AchievementServiceApplication.java
+++ AchievementService/src/main/java/io/duryskuba/interestmatcher/AchievementService/AchievementServiceApplication.java
@@ -2,7 +2,9 @@ package io.duryskuba.interestmatcher.AchievementService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AchievementServiceApplication {
 
